92cc8efc96b2a2f20754a0191a2ae019a598160c
Afzal Basha <afzalbasha992@gmail.com>
2023-05-10 15:47:05 +0530

    changes

--- servers.tf
+++ servers.tf
@@ -1,12 +1,13 @@
 
 
-#data "aws_ami" "centos8" {
-#  owners           = ["973714476881"]
-#  most_recent      = true
-#  name_regex       = "Centos-8-DevOps-practice"
-#}
-
-
+data "aws_ami" "example" {
+  owners           = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
+output "ami"{
+  value=data.aws_ami.example.image_id
+}
 
 
 resource "aws_instance" "frontend" {

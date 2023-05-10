d41d58b557aa85931c7f180eacc12b97448df01d
Afzal Basha <afzalbasha992@gmail.com>
2023-05-10 15:49:31 +0530

    changes

--- servers.tf
+++ servers.tf
@@ -5,22 +5,18 @@ data "aws_ami" "example" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
 }
-output "ami"{
-  value=data.aws_ami.example.image_id
-}
 
 
 resource "aws_instance" "frontend" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.example.image_id
   instance_type = "t3.micro"
 
   tags = {
     Name = "frontend"
   }
 }
-output "frontend"{
-  value=aws_instance.frontend.private_ip
-}
+
+
 
 
 #resource "aws_instance" "web" {

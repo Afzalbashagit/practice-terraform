a241c6304ba0199defdb4c44876ad169bab0477c
Afzal Basha <afzalbasha992@gmail.com>
2023-05-10 15:25:25 +0530

    changes

--- servers.tf
+++ servers.tf
@@ -1,8 +1,91 @@
-resource "aws_instance" "web" {
+
+
+
+
+
+
+
+resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "basha"
+    Name = "frontend"
   }
 }
+
+output "frontend"{
+  value=aws_instance.frontend.private_ip
+}
+#resource "aws_instance" "web" {
+#  ami           = "ami-0b5a2b5b8f2be4ec2"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "basha"
+#  }
+#}
+#resource "aws_instance" "web" {
+#  ami           = "ami-0b5a2b5b8f2be4ec2"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "basha"
+#  }
+#}
+#resource "aws_instance" "web" {
+#  ami           = "ami-0b5a2b5b8f2be4ec2"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "basha"
+#  }
+#}
+#resource "aws_instance" "web" {
+#  ami           = "ami-0b5a2b5b8f2be4ec2"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "basha"
+#  }
+#}
+#resource "aws_instance" "web" {
+#  ami           = "ami-0b5a2b5b8f2be4ec2"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "basha"
+#  }
+#}
+#resource "aws_instance" "web" {
+#  ami           = "ami-0b5a2b5b8f2be4ec2"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "basha"
+#  }
+#}
+#resource "aws_instance" "web" {
+#  ami           = "ami-0b5a2b5b8f2be4ec2"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "basha"
+#  }
+#}
+#resource "aws_instance" "web" {
+#  ami           = "ami-0b5a2b5b8f2be4ec2"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "basha"
+#  }
+#}
+#resource "aws_instance" "web" {
+#  ami           = "ami-0b5a2b5b8f2be4ec2"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "basha"
+#  }
+#}

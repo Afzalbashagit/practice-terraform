538c8311fd6b3493637b084bd564c02901e6d972
Afzal Basha <afzalbasha992@gmail.com>
2023-05-10 15:30:26 +0530

    changes

--- servers.tf
+++ servers.tf
@@ -1,8 +1,14 @@
 
 
+data "aws_ami" "centos" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-practice"
+  owners           = ["973714476881"]
+}
 
-
-
+output "ami"{
+  value=data.aws_ami.centos.image_id
+}
 
 
 resource "aws_instance" "frontend" {
@@ -14,9 +20,7 @@ resource "aws_instance" "frontend" {
   }
 }
 
-output "frontend"{
-  value=aws_instance.frontend.private_ip
-}
+
 #resource "aws_instance" "web" {
 #  ami           = "ami-0b5a2b5b8f2be4ec2"
 #  instance_type = "t3.micro"

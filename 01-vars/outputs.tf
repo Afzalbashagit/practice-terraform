7a9d46a5b8c3df88bdc340306eec5b95f2dad1ad
Afzal Basha <afzalbasha992@gmail.com>
2023-05-10 18:44:42 +0530

    changes

---
+++ 01-vars/outputs.tf
@@ -0,0 +1,8 @@
+
+output "sample_string"{
+  value=var.sample_string
+}
+
+output "sample_string1"{
+  value="Echo name:${var.sample_string}"
+}
--- 01-vars/vars.tf
+++ 01-vars/vars.tf
@@ -3,10 +3,3 @@
 variable "sample_string"{
   default="Afzal basha"
 }
-output "sample_string"{
-  value=var.sample_string
-}
-
-output "sample_string1"{
-  value="Echo name:${var.sample_string}"
-}

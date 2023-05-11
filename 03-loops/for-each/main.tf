cc1d690c8044b549fbd7d8a6efb6878f0a43f24c
Afzal Basha <afzalbasha992@gmail.com>
2023-05-11 11:03:59 +0530

    changes

---
+++ 03-loops/for-each/main.tf
@@ -0,0 +1,19 @@
+
+resource "null_resource" "fruits" {
+  for_each = var.fruits
+
+  provisioner "local-exec" {
+     command="Fruit name-${each.key},value-${each.value}"
+  }
+
+}
+
+
+
+variable "fruits"{
+  default={
+    apple=10
+    banana=34
+    mango=70
+  }
+}
